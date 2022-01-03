f97c709ca4473d4b8e1cf0eeff3ac8792f257331
Marcelo Pereira Santos <lemarcelo@outlook.com>
2022-01-03 00:56:20 -0300

    Correção de sintaxe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  luke = {name:'Luke', isJedi: true, temple:'Coruscant'}
+  leia = {name:'Leia', isJedi: false}
+  han = {name:'Han Solo', isJedi: false}
 }
